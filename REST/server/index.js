207d353d6525fd7d18c631427ac469de1e62765e
Taichir0923 <crystal19950923@gmail.com>
2021-10-23 14:58:30 +0800

    aPi

--- REST/server/index.js
+++ REST/server/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const bp = require('body-parser')
+const bp = require('body-parser');
+
 
 const userRoutes = require('./routes/usersRouter');
 
+require('dotenv').config()
 app.use(cors());
 
 app.use(bp.json());
 
 app.use(userRoutes);
 
-mongoose.connect('mongodb+srv://narada:admin1@cluster0.wtu5m.mongodb.net/rest')
+mongoose.connect(process.env.MONGO_URI)
 app.listen(3000, () => {
     console.log('server started on port 3000')
 })
