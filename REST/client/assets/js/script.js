ae26b5dedb0aeb70ba77704308367704d12dc6bb
Taichir0923 <crystal19950923@gmail.com>
2021-10-23 15:51:29 +0800

    swal update

--- REST/client/assets/js/script.js
+++ REST/client/assets/js/script.js
@@ -6,34 +6,58 @@ const password = document.querySelector('#password');
 
 submit.addEventListener('click', async e => {
     e.preventDefault();
-    const formData = {
-        username: username.value,
-        email: email.value,
-        number: number.value,
-        password: password.value
-    }
 
-    try {
-        const response = await fetch('http://localhost:3000/register', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        })
+    if(username.value.trim() !== '' || email.value.trim() !== '' || number.value.trim() !== '' || password.value.trim() !== ''){
 
-        if(!response.ok){
-            throw Error('Хэрэглэгч бүртгэхэд алдаа гарлаа...')
+        const formData = {
+            username: username.value,
+            email: email.value,
+            number: number.value,
+            password: password.value
         }
     
-        const result = await response.json();
-        alert(result.message);
-    } catch (error) {
-        alert(error.message)
+        try {
+            const response = await fetch('http://localhost:3000/register', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+    
+            if(!response.ok){
+                throw Error('Хэрэглэгч бүртгэхэд алдаа гарлаа...')
+            }
+        
+            const result = await response.json();
+            swal({
+                icon: 'success',
+                text: result.message
+            });
+        } catch (error) {
+            swal({
+                icon: 'warning',
+                text: error.message
+            })
+        }
+
+        resetForm()
+    } else {
+        swal({
+            icon: 'error',
+            text: "Бүх талбаруудыг бөглөнө үү..."
+        })
     }
 
 
 })
+
+function resetForm(){
+    username.value = '';
+    email.value = '';
+    number.value = '';
+    password.value = '';
+}
 
 fetch('http://localhost:3000/allUsers')
     .then(res => res.json())
