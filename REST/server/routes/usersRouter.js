f5536f5f9c2c433f5f855b501d535a1f4d41aa84
Taichir0923 <crystal19950923@gmail.com>
2021-10-24 14:01:31 +0800

    update

--- REST/server/routes/usersRouter.js
+++ REST/server/routes/usersRouter.js
@@ -5,6 +5,6 @@ const userController = require('../controllers/usersController');
 router.post('/register', userController.registerUser);
 router.get('/allUsers', userController.getAllUsers);
 router.post('/updateUser/:id', userController.updateUser);
-router.get('singleUser/:id', userController.singleUser);
+router.get('/singleUser/:id', userController.singleUser);
 
 module.exports = router;
