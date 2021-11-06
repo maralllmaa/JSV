67d5970e9d94f9a998da8910f38c73b76a818718
Taichir0923 <crystal19950923@gmail.com>
2021-11-06 14:31:57 +0800

    asd

--- complete/src/components/App.js
+++ complete/src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Card from "./Card";
 import Container from "./Container";
+import Form from "./Form";
 
 const App = () => {
   return (
     <React.Fragment>
         <Container>
             <Card>
-                
+                <Form>
+
+                </Form>
             </Card>
         </Container>
     </React.Fragment>
--- complete/src/components/Label.js
+++
@@ -1,9 +0,0 @@
-const Form = props => {
-    return <form className='w-96' onSubmit={props.onSubmit}>
-        {
-            props.children
-        }
-    </form>
-}
-
-export default Form;
