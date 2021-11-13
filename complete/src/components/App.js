6e812bfc50906118e45129548d2d589b9ad0dde2
Taichir0923 <crystal19950923@gmail.com>
2021-11-13 16:03:36 +0800

    Update App.js

--- complete/src/components/App.js
+++ complete/src/components/App.js
@@ -1,4 +1,4 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import Container from "./Container";
 import Form from "./Form";
@@ -10,82 +10,138 @@ import Divider from "./Divider";
 import swal from "sweetalert";
 
 const App = () => {
-    const [ username , setUsername ] = useState('');
-    const [ email , setEmail ] = useState('');
-    const [ number , setNumber ] = useState('');
-    const [ password , setPassword ] = useState('');
-    const [ userList , setUserList ] = useState([]);
-    const [ edit , setEdit ] = useState(false);
-    const [ userId , setUserId ] = useState(null);
-    const submitHandler = e => {
-        e.preventDefault();
-        if(username.trim() !== '' && email.trim() !== '' && number.trim() !== '' && password.trim() !== ''){
-            setUserList([...userList, {
-                username: username,
-                email: email,
-                number: number,
-                password: password,
-                id: Math.random().toString().split('.')[1]
-            }])
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [userList, setUserList] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (
+      username.trim() !== "" &&
+      email.trim() !== "" &&
+      number.trim() !== "" &&
+      password.trim() !== ""
+    ) {
+      setUserList([
+        ...userList,
+        {
+          username: username,
+          email: email,
+          number: number,
+          password: password,
+          id: Math.random().toString().split(".")[1],
+        },
+      ]);
 
-            swal({
-                icon: 'success',
-                text: 'Амжилттай бүртгэгдлээ'
-            })
-            resetForm();
-        } else {
-            swal({
-                icon: 'error',
-                text: 'Бүх мэдээллийг оруулах шаардлагатай'
-            })
-        }
+      swal({
+        icon: "success",
+        text: "Амжилттай бүртгэгдлээ",
+      });
+      resetForm();
+    } else {
+      swal({
+        icon: "error",
+        text: "Бүх мэдээллийг оруулах шаардлагатай",
+      });
     }
+  };
 
-    function resetForm(){
-        setUsername('')
-        setEmail('')
-        setNumber('')
-        setPassword('')
+  const updateHandler = e => {
+    e.preventDefault();
+    const userIndex = userList.findIndex(user => user.id === userId);
+    const updateUserData = [...userList];
+    updateUserData[userIndex] = {
+        username: username,
+        email: email,
+        number: number,
+        password: password,
+        id: userId
     }
+    setUserList(updateUserData);
+    setEdit(false);
+    resetForm();
+  }
 
-    const deleteHandler = id => {
-        setUserList(userList.filter(user => user.id !== id))
-    }
+  function resetForm() {
+    setUsername("");
+    setEmail("");
+    setNumber("");
+    setPassword("");
+  }
 
-    const getData = id => {
-        setUserId(id);
-        setEdit(true);
-        const userInfo = userList.find(user => user.id === id);
-        setUsername(userInfo.username);
-    }
+  const deleteHandler = (id) => {
+    setUserList(userList.filter((user) => user.id !== id));
+    setEdit(false);
+    resetForm();
+  };
 
-    return (
-        <React.Fragment>
-            <Container>
-                <Card>
-                    <Form onSubmit={submitHandler}>
-                        <Label htmlFor="username" label="Username" />
-                        <Input onChange={e => setUsername(e.target.value)} value={username} id="username" placeholder="Username" />
-                        <Label htmlFor="email" label="Email" />
-                        <Input onChange={e => setEmail(e.target.value)} value={email} id="email" placeholder="Email"  />
-                        <Label htmlFor="number" label="Number" />
-                        <Input onChange={e => setNumber(e.target.value)} value={number} id="number" placeholder="Number" type="number" />
-                        <Label htmlFor="password" label="Password" />
-                        <Input onChange={e => setPassword(e.target.value)} value={password} id="password" placeholder="Password" type="password" />
-                        <Button val={
-                            edit ? "Засах" : "Бүртгэх"
-                        } bg="green" />
-                    </Form>
-                </Card>
+  const getData = (id) => {
+    setUserId(id);
+    setEdit(true);
+    const userInfo = userList.find((user) => user.id === id);
+    setUsername(userInfo.username);
+    setEmail(userInfo.email);
+    setNumber(userInfo.number);
+    setPassword(userInfo.username);
+  };
 
-                <Divider />
+  return (
+    <React.Fragment>
+      <Container>
+        <Card>
+          <Form onSubmit={edit ? updateHandler : submitHandler}>
+            <Label htmlFor="username" label="Username" />
+            <Input
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              id="username"
+              placeholder="Username"
+            />
+            <Label htmlFor="email" label="Email" />
+            <Input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              id="email"
+              placeholder="Email"
+            />
+            <Label htmlFor="number" label="Number" />
+            <Input
+              onChange={(e) => setNumber(e.target.value)}
+              value={number}
+              id="number"
+              placeholder="Number"
+              type="number"
+            />
+            <Label htmlFor="password" label="Password" />
+            <Input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              id="password"
+              placeholder="Password"
+              type="password"
+            />
+            <Button val={edit ? "Засах" : "Бүртгэх"} bg="green" />
+          </Form>
+        </Card>
 
-                {userList.length !== 0 && <UserList getEditData={getData} deleteUser={deleteHandler} data={userList} />}
-            </Container>
-        </React.Fragment>
-    );
+        <Divider />
+
+        {userList.length !== 0 && (
+          <UserList
+            getEditData={getData}
+            deleteUser={deleteHandler}
+            data={userList}
+          />
+        )}
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default App;
 
 // Edit
+// Article 
